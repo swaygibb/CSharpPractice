fa0c747c811670d82f1a768f9f349023e72839c9
Steve Gibbs <swaygibb@gmail.com>
2025-05-12 10:28:22 -0500

    Added extension methods

---
+++ Extensions/MeetExtensions.cs
@@ -0,0 +1,12 @@
+﻿using CSharpPractice.Models;
+
+namespace CSharpPractice.Extensions
+{
+    public static class MeetExtensions
+    {
+        public static string FormatDate(this DateTime? date, string format = "MMMM dd, yyyy")
+        {
+            return date?.ToString(format) ?? "No Date";
+        }
+    }
+}
---
+++ Extensions/ResultExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpPractice.Extensions
+{
+    public static class ResultExtensions
+    {
+        public static string ToSlug(this string name)
+        {
+            return name.ToLower().Replace(" ", "-");
+        }
+    }
+}
